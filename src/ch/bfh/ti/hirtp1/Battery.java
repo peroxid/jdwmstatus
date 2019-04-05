f62edfbe994343321926bd36c288c7a7d488daa9
Patrick Hirt <patrick.hirt@gmail.com>
2019-04-05 14:00:20 +0200

    updated gitignore for intellij

---
+++ src/ch/bfh/ti/hirtp1/Battery.java
@@ -0,0 +1,119 @@
+package ch.bfh.ti.hirtp1;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+
+public class Battery {
+    private final Logger LOG = Logger.getLogger(Battery.class.getCanonicalName());
+    private final static String BATTERY_BASE_PATH = "/sys/class/power_supply";
+    private final Path myPath;
+    private int energyNowPrevious;
+    private final int energyFull;
+
+
+
+    public Battery(final Path path) {
+        this.myPath = Paths.get(Battery.BATTERY_BASE_PATH, path.toString());
+        this.energyNowPrevious = this.getEnergyNow().orElse(0);
+        this.energyFull = this.getEnergyFull().orElse(0);
+    }
+
+    public static List<Battery> listBatteries() {
+        return  Arrays.stream(
+                Objects.requireNonNull(new File(Battery.BATTERY_BASE_PATH).list(
+                        (file, s) -> s.length() == 4 && s.startsWith("BAT")
+                ))
+        ).map(i -> new Battery(Paths.get(i))).collect(Collectors.toList());
+    }
+
+    public static char getChargingDischargingSymbol(final boolean isCharging) {
+        return isCharging ? '+' : '-';
+    }
+
+    public int minutesUntilEmptyOrFull(final int intervalInMilliseconds) {
+        final Optional<Integer> energyNow = this.getEnergyNow();
+
+        if(energyNow.isPresent()) {
+            final int remainingEnergy = this.energyFull - energyNow.get();
+            final int difference = (energyNow.get() - this.energyNowPrevious) / (intervalInMilliseconds * 1000 * 60) ;
+            this.energyNowPrevious = energyNow.get();
+            if(difference == 0) {
+                return 0;
+            }
+
+            return Math.abs(new Double((((double) remainingEnergy / difference))).intValue());
+        }
+
+        return 0;
+    }
+
+
+    public static String hoursAndMinutesUntilEmptyOrFull(final int minutesUntilEmptyOrFull) {
+        return String.format("%02d:%02d", (minutesUntilEmptyOrFull / 60), (minutesUntilEmptyOrFull % 60));
+    }
+
+    public boolean isCharging() {
+        try {
+            final String contents = Files.readAllLines(Paths.get(this.myPath.toString(), "status")).get(0);
+            if(contents.startsWith("Charging")) {
+                return true;
+            }
+            else if(contents.startsWith("Discharging")) {
+                return false;
+            }
+        } catch (IOException e) {
+            this.LOG.log(Level.SEVERE, String.format("Unable to read file %s: %s", this.myPath.toString(), e.getMessage()), e);
+        }
+        return false;
+    }
+
+    public Optional<Integer> getChargePercentage() {
+        final Optional<Integer> energyFull = this.getEnergyFull();
+        final Optional<Integer> energyNow = this.getEnergyNow();
+        if(energyFull.isPresent() && energyNow.isPresent()) {
+            return Optional.of(new Double(((double) energyNow.get() / energyFull.get()) * 100).intValue());
+        }
+        this.LOG.log(Level.WARNING, "Unable to detect charge percentage");
+        return Optional.empty();
+    }
+
+
+    private Optional<Integer> getIntInFileByKey(final Path path, final String key) {
+        try (final BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                if(line.startsWith(key)) {
+                    return Optional.of(Integer.parseInt(line.substring(line.indexOf("=") + 1)));
+                }
+            }
+        } catch (IOException e) {
+            this.LOG.log(Level.SEVERE, String.format("Unable to read file %s: %s", path.toString(), e.getMessage()), e);
+            return Optional.empty();
+        }
+
+        this.LOG.log(Level.SEVERE, String.format("Found no line starting with %s in file %s", key, path.toString()));
+        return Optional.empty();
+    }
+
+    private Optional<Integer> getEnergyFull() {
+        return this.getIntInFileByKey(Paths.get(this.myPath.toString(), "uevent"), "POWER_SUPPLY_ENERGY_FULL");
+    }
+
+    private Optional<Integer> getEnergyNow() {
+        return this.getIntInFileByKey(Paths.get(this.myPath.toString(), "uevent"), "POWER_SUPPLY_ENERGY_NOW");
+    }
+
+}
---
+++ src/ch/bfh/ti/hirtp1/Main.java
@@ -0,0 +1,98 @@
+package ch.bfh.ti.hirtp1;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+
+    private OperatingSystemMXBean operatingSystemMXBean;
+    private final List<Battery> batteries;
+    private DateTimeFormatter dateTimeFormatter;
+    private static final int UPDATE_INTERVAL_IN_MILLISECONDS = 1000 * 60;
+
+    public static void main(String[] args) {
+        final Main me = new Main();
+
+        try {
+            //noinspection InfiniteLoopStatement
+            while(true)
+            {
+                System.out.println(me.getStatusLine());
+                Thread.sleep(Main.UPDATE_INTERVAL_IN_MILLISECONDS);
+            }
+        } catch (InterruptedException ignored) {}
+    }
+
+    public Main() {
+        this.batteries = Battery.listBatteries();
+    }
+
+    public String getStatusLine() {
+        return String.format(
+                "T:%d°C L:%.2f B:%d%%%c %s|%s",
+                this.getTemperatureInCelsius(Paths.get("/sys/devices/virtual/thermal/thermal_zone5/temp")),
+                this.getLoadAvg(),
+                (this.batteries.get(0).getChargePercentage().orElse(0) + this.batteries.get(1).getChargePercentage().orElse(0)),
+                Battery.getChargingDischargingSymbol(this.batteries.get(0).isCharging() || this.batteries.get(1).isCharging()),
+                Battery.hoursAndMinutesUntilEmptyOrFull(
+                        this.batteries.get(0).minutesUntilEmptyOrFull() +
+                                this.batteries.get(1).minutesUntilEmptyOrFull()
+                ),
+                this.getTimeAndDate()
+        );
+    }
+
+    private int getTemperatureInCelsius(final Path path) {
+        return getIntInFile(path).orElse(1000)/1000;
+    }
+
+    private double getLoadAvg() {
+        if(this.operatingSystemMXBean == null) {
+            this.operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+        }
+        return this.operatingSystemMXBean.getSystemLoadAverage();
+    }
+
+    private String getTimeAndDate() {
+        if(this.dateTimeFormatter == null) {
+            this.dateTimeFormatter = new DateTimeFormatterBuilder()
+                    .appendPattern("HH")
+                    .appendLiteral(":")
+                    .appendPattern("mm")
+                    .appendLiteral(" ")
+                    .appendLiteral("KW")
+                    .appendPattern("w")
+                    .appendLiteral(" ")
+                    .appendPattern("dd")
+                    .appendLiteral(".")
+                    .appendPattern("MM")
+                    .appendLiteral(".")
+                    .appendPattern("YYYY")
+                    .toFormatter();
+        }
+        return ZonedDateTime.now().format(this.dateTimeFormatter);
+    }
+
+    public static Optional<Integer> getIntInFile(final Path path) {
+        try (final BufferedReader reader = Files.newBufferedReader(path)) {
+            return Optional.of(Integer.parseInt(reader.readLine()));
+        } catch (IOException e) {
+            System.err.println(String.format("Unable to read file %s: %s", path.toString(), e.getMessage()));
+        }
+
+        return Optional.empty();
+
+    }
+
+}
